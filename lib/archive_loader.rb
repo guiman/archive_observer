47eaba9c5998601e43d3c706e5c2a55db95fd729
Alvaro F. Lara <alvaro.lara@alliants.com>
2015-11-03 20:59:32 +0000

    Fix extra field not present in table

--- lib/archive_loader.rb
+++ lib/archive_loader.rb
@@ -10,7 +10,7 @@ class ArchiveLoader
 
     user_insert_template = "INSERT INTO github_users (login, created_at, updated_at) VALUES ('%{login}',now(),now())"
     language_insert_template = "INSERT INTO languages (name,created_at,updated_at) VALUES ('%{name}', now(),now());"
-    repository_insert_template = "INSERT INTO github_repositories (full_name,language_id,fork,created_at,updated_at) VALUES ('%{full_name}', (SELECT id FROM languages WHERE languages.name ILIKE '%{repository_language}'), %{fork}, now(),now());"
+    repository_insert_template = "INSERT INTO github_repositories (full_name,language_id,created_at,updated_at) VALUES ('%{full_name}', (SELECT id FROM languages WHERE languages.name ILIKE '%{repository_language}'), now(),now());"
     pr_insert_template = "INSERT INTO github_pull_requests (action,merged,original_id,event_timestamp,github_user_id, github_repository_id,created_at,updated_at) VALUES ('%{action}','%{merged}', %{original_id}, '%{event_timestamp}', (SELECT id FROM github_users WHERE github_users.login = '%{user_login}'), (SELECT id FROM github_repositories WHERE github_repositories.full_name = '%{repository_full_name}'), now(),now());"
 
     user_inserts = []
@@ -34,7 +34,6 @@ class ArchiveLoader
       }
       repository_insert_data = {
         full_name: repo.fetch("full_name"),
-        fork: repo.fetch("fork"),
         repository_language: language_insert_data.fetch(:name)
       }
       pr_insert_data = {
