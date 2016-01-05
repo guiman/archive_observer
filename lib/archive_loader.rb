0760fe7d83cf6a99c800041d977d73728809a757
Alvaro F. Lara <alvaro.lara@alliants.com>
2016-01-05 22:42:38 +0000

    Refactor loader with some helper clases

--- lib/archive_loader.rb
+++ lib/archive_loader.rb
@@ -3,16 +3,110 @@ require 'open-uri'
 require 'zlib'
 require 'yajl'
 
+class LanguageHandler
+  TEMPLATE = "INSERT INTO languages (name,created_at,updated_at) VALUES ('%{name}', now(),now());"
+
+  def initialize(data)
+    @data = data.fetch("payload").fetch("pull_request").fetch("head").fetch("repo")
+  end
+
+  def name
+    language_name = @data.fetch("language", "Unknown") || "Unknown"
+    (language_name.empty? || language_name == "null")  ? "Unknown" : language_name
+  end
+
+  def sql
+    TEMPLATE % { name: name }
+  end
+end
+
+class UserHandler
+  TEMPLATE = "INSERT INTO github_users (login, created_at, updated_at) VALUES ('%{login}',now(),now())"
+
+  def initialize(data)
+    @data = data.fetch("actor")
+  end
+
+  def login
+    @data.fetch("login")
+  end
+
+  def sql
+    TEMPLATE % { login: login }
+  end
+end
+
+class RepositoryHandler
+  TEMPLATE = "INSERT INTO github_repositories (full_name,language_id,created_at,updated_at) VALUES ('%{full_name}', (SELECT id FROM languages WHERE languages.name ILIKE '%{repository_language}'), now(),now());"
+  attr_reader :language
+
+  def initialize(data, language)
+    @data = data.fetch("payload").fetch("pull_request").fetch("head").fetch("repo")
+    @language = language
+  end
+
+  def full_name
+    @data.fetch("full_name")
+  end
+
+  def sql
+    TEMPLATE % {
+      full_name: full_name,
+      repository_language: language.name
+    }
+  end
+end
+
+class PullRequestHandler
+  TEMPLATE = "INSERT INTO github_pull_requests (action,merged,original_id,event_timestamp,github_user_id, github_repository_id,created_at,updated_at) VALUES ('%{action}','%{merged}', %{original_id}, '%{event_timestamp}', (SELECT id FROM github_users WHERE github_users.login = '%{user_login}'), (SELECT id FROM github_repositories WHERE github_repositories.full_name = '%{repository_full_name}'), now(),now());"
+  attr_reader :user, :repository
+
+  def initialize(data, repository, user)
+    @data = data.fetch("payload")
+    @repository = repository
+    @user = user
+  end
+
+  def action
+    @data.fetch("action")
+  end
+
+  def merged?
+    @data.fetch("pull_request").fetch("merged")
+  end
+
+  def original_id
+    @data.fetch("pull_request").fetch("id")
+  end
+
+  def event_timestamp
+    @data.fetch("pull_request").fetch("updated_at")
+  end
+
+  def sql
+    TEMPLATE % {
+      action: action,
+      merged: merged?,
+      user_login: user.login,
+      repository_full_name: repository.full_name,
+      original_id: original_id,
+      event_timestamp: event_timestamp
+    }
+  end
+end
+
 class ArchiveLoader
   def self.perform(timestamp)
     gz = open("http://data.githubarchive.org/#{timestamp}.json.gz")
     js = Zlib::GzipReader.new(gz).read
 
-    user_insert_template = "INSERT INTO github_users (login, created_at, updated_at) VALUES ('%{login}',now(),now())"
-    language_insert_template = "INSERT INTO languages (name,created_at,updated_at) VALUES ('%{name}', now(),now());"
-    repository_insert_template = "INSERT INTO github_repositories (full_name,language_id,created_at,updated_at) VALUES ('%{full_name}', (SELECT id FROM languages WHERE languages.name ILIKE '%{repository_language}'), now(),now());"
-    pr_insert_template = "INSERT INTO github_pull_requests (action,merged,original_id,event_timestamp,github_user_id, github_repository_id,created_at,updated_at) VALUES ('%{action}','%{merged}', %{original_id}, '%{event_timestamp}', (SELECT id FROM github_users WHERE github_users.login = '%{user_login}'), (SELECT id FROM github_repositories WHERE github_repositories.full_name = '%{repository_full_name}'), now(),now());"
+    import_by_the_old_method(js)
 
+    ArchiveExtensions::RepositoryRankingUpdate.update
+    ArchiveExtensions::UserRankingUpdate.update
+  end
+
+  def self.import_by_the_old_method(js)
     user_inserts = []
     languages_inserts = []
     repository_inserts = []
@@ -24,33 +118,15 @@ class ArchiveLoader
         next
       end
 
-      repo = event.fetch("payload").fetch("pull_request").fetch("head").fetch("repo")
+      language = LanguageHandler.new(event)
+      user = UserHandler.new(event)
+      repository = RepositoryHandler.new(event, language)
+      pull_request = PullRequestHandler.new(event, repository, user)
 
-      user_insert_data = {
-        login: event.fetch("actor").fetch("login")
-      }
-
-      language_name = repo.fetch("language", "Unknown") || "Unknown"
-      language_insert_data = {
-        name: (language_name.empty? || language_name == "null")  ? "Unknown" : language_name
-      }
-      repository_insert_data = {
-        full_name: repo.fetch("full_name"),
-        repository_language: language_insert_data.fetch(:name)
-      }
-      pr_insert_data = {
-        action: event.fetch("payload").fetch("action"),
-        merged: event.fetch("payload").fetch("pull_request").fetch("merged"),
-        user_login: user_insert_data.fetch(:login),
-        repository_full_name: repository_insert_data.fetch(:full_name),
-        original_id: event.fetch("payload").fetch("pull_request").fetch("id"),
-        event_timestamp: event.fetch("payload").fetch("pull_request").fetch("updated_at")
-      }
-
-      user_inserts << user_insert_template % user_insert_data
-      languages_inserts << language_insert_template % language_insert_data
-      repository_inserts << repository_insert_template % repository_insert_data
-      pr_inserts << pr_insert_template % pr_insert_data
+      user_inserts << user.sql
+      languages_inserts << language.sql
+      repository_inserts << repository.sql
+      pr_inserts << pull_request.sql
     end
 
     user_inserts.each do |insert|
@@ -80,8 +156,5 @@ class ArchiveLoader
       rescue
       end
     end
-
-    ArchiveExtensions::RepositoryRankingUpdate.update
-    ArchiveExtensions::UserRankingUpdate.update
   end
 end
