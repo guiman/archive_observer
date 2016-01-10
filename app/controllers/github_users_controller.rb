1c800976c534f8b2761be14714f6487b5b44340c
Alvaro F. Lara <alvaro.lara@alliants.com>
2016-01-10 20:30:14 +0000

    Sneaky year filtering

--- app/controllers/github_users_controller.rb
+++ app/controllers/github_users_controller.rb
@@ -5,6 +5,7 @@ class GithubUsersController < ApplicationController
 
   def profile
     @user = GithubUser.find_by_login(params[:login])
+    @year = (params[:year] || Time.now.year).to_i
     if @user
       render "profile"
     else
