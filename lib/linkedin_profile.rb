fa2e089c1dff22cba209d621bcb5604f5e436130
Alvaro F. Lara <alvaro.lara@alliants.com>
2016-01-13 07:08:13 +0000

    Attempt to improve the chances of discovering the linkedin.
    The problem is that github users put their linkedin profile as their personal
    site link, and I follow that link in order to find a reference to their linkedin
    link in their personal sites using Nokogiri.
    
    Now I verify first if the "personal site" link is actually the "link I'm looking"
    for (jedi mind tricks aside).
    
    Also took the chance to remove all the other methods I don't intend on using.

--- lib/linkedin_profile.rb
+++ lib/linkedin_profile.rb
@@ -22,14 +22,20 @@ class LinkedinProfile
 
       is_a_github_url = /(http|https):\/\/github.com.*/
       is_a_url =  /(http|https):\/\/.*/
+      is_a_linkedin_url = personal_site.include?('linkedin')
 
       return if (personal_site =~ is_a_github_url)
       return unless (personal_site =~ is_a_url)
 
-      # And then we search in their personal site
-      doc = Nokogiri::HTML(open(personal_site, allow_redirections: :safe, read_timeout: 1, open_timeout: 1))
-      links = doc.xpath('//a[@href]').map {|link| link["href"] }
-      linkedin_link = links.detect { |a| a.include?('linkedin') }
+      if is_a_linkedin_url
+        # For people putting their linkedin profile as personal site
+        linkedin_link = personal_site
+      else
+        # And then we search in their personal site
+        doc = Nokogiri::HTML(open(personal_site, allow_redirections: :safe, read_timeout: 1, open_timeout: 1))
+        links = doc.xpath('//a[@href]').map {|link| link["href"] }
+        linkedin_link = links.detect { |a| a.include?('linkedin') }
+      end
 
       @processed = true
       @linkedin_link = linkedin_link
@@ -41,79 +47,5 @@ class LinkedinProfile
   def verify_link
     return false unless link
     !(link =~ /(http|https):\/\/.*linkedin\.com/).nil?
-  end
-
-  def headline
-    return "" unless link
-
-    begin
-      @page ||= Nokogiri::HTML(open(link, allow_redirections: :safe))
-      @page.css('div#headline').text
-    rescue OpenURI::HTTPError => e
-      Rails.logger.error("Found an error while processing linkedin profile for #{@login} -> #{e.to_s}")
-      return ""
-    end
-  end
-
-  def skills
-    return [] unless link
-
-    begin
-      @page ||= Nokogiri::HTML(open(link, allow_redirections: :safe))
-      @page.css('span.endorse-item-name').map { |e| e.text }
-    rescue OpenURI::HTTPError => e
-      Rails.logger.error("Found an error while processing linkedin profile for #{@login} -> #{e.to_s}")
-      return []
-    end
-  end
-
-  def education
-    return {} unless link
-
-    begin
-      @page = Nokogiri::HTML(open(link, allow_redirections: :safe))
-      education_element = @page.css('div#background-education')
-      return {} if education_element.empty?
-
-      university = education_element.css('div.education.first header h4').text
-      degree = education_element.css('div.education.first header h5 span').map(&:text).join('')
-      { university: university, degree: degree }
-    rescue OpenURI::HTTPError => e
-      Rails.logger.error("Found an error while processing linkedin profile for #{@login} -> #{e.to_s}")
-      return {}
-    end
-  end
-
-  def experience
-    return [] unless link
-
-    begin
-      @page ||= Nokogiri::HTML(open(link, allow_redirections: :safe))
-      experience_element = @page.css('div#background-experience')
-      experience = []
-
-      experience_element.css('div.current-position').each do |current_position_element|
-        experience << {
-          position: current_position_element.css('header h4').text,
-          company: (current_position_element.css('header h5 a').text.empty?) ? current_position_element.css('header span').text : current_position_element.css('header h5 a').text,
-          period: experience_element.css('div.current-position span.experience-date-locale').text,
-          current: true
-        }
-      end
-
-      experience_element.css('div.past-position').each do |position_element|
-        experience << {
-          position: position_element.css('header h4').text,
-          company: (position_element.css('header h5 a').text.empty?) ? position_element.css('header span').text : position_element.css('header h5 a').text,
-          period: position_element.css('span.experience-date-locale').text,
-          current: false
-        }
-      end
-
-      experience
-    rescue OpenURI::HTTPError => e
-      Rails.logger.error("Found an error while processing linkedin profile for #{@login} -> #{e.to_s}")
-      return []
-    end
   end
 end
