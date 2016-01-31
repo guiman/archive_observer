5d5f4d8cb02cadc7e19577a1aa5b4614eaca206f
Alvaro F. Lara <alvaro.lara@alliants.com>
2016-01-31 21:08:43 +0000

    Contributors to all versions of the repo

--- app/models/github_repository.rb
+++ app/models/github_repository.rb
@@ -2,10 +2,14 @@ class GithubRepository < ActiveRecord::Base
   has_many :github_pull_requests
   belongs_to :language
 
+  def partial_name
+    full_name.split('/').last
+  end
+
   def contributors
     GithubUser.joins(pull_requests: :github_repository).
       select("github_users.login, count(github_pull_requests.id) as prs, github_repositories.id").
-      where("github_repositories.id = :repo_id", repo_id: self.id).
+      where("github_repositories.full_name ~ '.*\/#{self.partial_name}$'").
       group("github_users.login, github_repositories.id").
       order("prs desc").
       distinct
