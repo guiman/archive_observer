4dca722929545b13377d6b10cd9b89eb36cf6ece
Alvaro F. Lara <alvaro.lara@alliants.com>
2016-02-05 22:13:58 +0000

    Allow to switch years

--- app/controllers/github_users_controller.rb
+++ app/controllers/github_users_controller.rb
@@ -6,6 +6,8 @@ class GithubUsersController < ApplicationController
   def profile
     @user = GithubUser.find_by_login(params[:login])
     @year = (params[:year] || Time.now.year).to_i
+    @alternative_year = ([Time.now.year - 1, Time.now.year] - [@year]).first
+
     if @user
       render "profile"
     else
