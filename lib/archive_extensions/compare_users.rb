d710ed339a4440b332fe5d4495903fbb51eaa707
Alvaro F. Lara <alvaro.lara@alliants.com>
2016-02-07 21:59:14 +0000

    Reduce the queries to one, get a speedup of half the time

--- lib/archive_extensions/compare_users.rb
+++ lib/archive_extensions/compare_users.rb
@@ -4,6 +4,44 @@ module ArchiveExtensions
   class CompareUsers
     COMPARED_LANGUAGES = 4
 
+    def self.calculate_language_similarity_new(user_a:, user_b:)
+      user_a_id = GithubUser.find_by(login: user_a).id
+      user_b_id = GithubUser.find_by(login: user_b).id
+
+      user_ids = [user_a_id, user_b_id].join(',')
+
+      results = Language.connection.select_all("
+          SELECT github_pull_requests.github_user_id as user_id, languages.name as language_name, count(github_pull_requests.id) as prs
+          FROM languages
+          INNER JOIN github_repositories on github_repositories.language_id = languages.id
+          INNER JOIN github_pull_requests on github_pull_requests.github_repository_id = github_repositories.id
+          WHERE github_pull_requests.github_user_id in (#{user_ids}) AND github_pull_requests.action = 'opened'
+          GROUP BY github_pull_requests.github_user_id, languages.name
+          ORDER BY prs desc")
+
+      data = results.inject([]) do |acc, result|
+        pr_count = result.fetch("prs")
+        acc << {
+          "user_id" => result.fetch("user_id").to_i,
+          "language" => result.fetch("language_name"),
+          "prs" => pr_count.to_i
+        }
+      end
+
+      user_a_languages = data.map do |some_data|
+        some_data.fetch("language") if some_data.fetch("user_id") == user_a_id
+      end.compact.first(COMPARED_LANGUAGES)
+
+      user_b_languages = data.map do |some_data|
+        some_data.fetch("language") if some_data.fetch("user_id") == user_b_id
+      end.compact.first(COMPARED_LANGUAGES)
+
+      whole_list_of_languages = (user_a_languages | user_b_languages).count
+      language_intersection = (user_a_languages & user_b_languages).count
+
+      language_intersection.to_f / whole_list_of_languages.to_f * 100.to_f
+    end
+
     def self.calculate_language_similarity(user_a:, user_b:)
       user_a_languages = ArchiveExtensions::Languages.for(user_a).
         first(COMPARED_LANGUAGES).map {|x| x.fetch("language").name }
